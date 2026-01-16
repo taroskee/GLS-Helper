1d0174306f8054b0e4425e13900b018cd9e16c23
matsudaira <matsudaira.taro@megachips.co.jp>
2026-01-16 08:24:23 +0000

    fix: REFACTOR more mean and less std

--- src/infra/repository/sqlite_graph_repository.py
+++ src/infra/repository/sqlite_graph_repository.py
@@ -4,15 +4,14 @@ from src.domain.model.edge import Edge
 from src.domain.model.node import Node
 from src.domain.protocol.graph_repository import GraphRepository
 
-_QUERIES_SETUP = tuple(
-    [
-        """
-     CREATE TABLE IF NOT EXISTS nodes (
+_SQLS_SETUP: tuple[str, ...] = (
+    """
+    CREATE TABLE IF NOT EXISTS nodes (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT UNIQUE NOT NULL
     )
     """,
-        """
+    """
     CREATE TABLE IF NOT EXISTS edges (
         src TEXT NOT NULL,
         dst TEXT NOT NULL,
@@ -20,8 +19,13 @@ _QUERIES_SETUP = tuple(
         delay_fall REAL
     )
     """,
-    ]
 )
+
+_SQL_INSERT_NODE: str = "INSERT OR IGNORE INTO nodes (name) VALUES (?)"
+_SQL_INSERT_EDGE: str = """
+    INSERT INTO edges (src, dst, delay_rise, delay_fall)
+    VALUES (?, ?, ?, ?)
+"""
 
 
 class SqliteGraphRepository(GraphRepository):
@@ -29,9 +33,17 @@ class SqliteGraphRepository(GraphRepository):
         self.db_path = db_path
 
     def setup(self) -> None:
-        """Orchestrates the database schema initialization."""
+        """
+        Orchestrates the database schema initialization.
+        Also applies performance tuning for massive write operations.
+        """
         with sqlite3.connect(self.db_path) as connection:
-            for script in _QUERIES_SETUP:
+            # WAL mode: To improve multi-thred in writing
+            connection.execute("PRAGMA journal_mode = WAL")
+            # NORMAL mode: To improve writing but less stable
+            connection.execute("PRAGMA synchronous = NORMAL")
+
+            for script in _SQLS_SETUP:
                 connection.execute(script)
 
     def save_nodes_batch(self, nodes: list[Node]) -> None:
@@ -42,9 +54,7 @@ class SqliteGraphRepository(GraphRepository):
         data = [(n.name,) for n in nodes]
 
         with sqlite3.connect(self.db_path) as connection:
-            connection.executemany(
-                "INSERT OR IGNORE INTO nodes (name) VALUES (?)", data
-            )
+            connection.executemany(_SQL_INSERT_NODE, data)
 
     def save_edges_batch(self, edges: list[Edge]) -> None:
         """Save a batch of edges using fast executemany."""
@@ -54,10 +64,4 @@ class SqliteGraphRepository(GraphRepository):
         data = [(e.src_node, e.dst_node, e.delay_rise, e.delay_fall) for e in edges]
 
         with sqlite3.connect(self.db_path) as connection:
-            connection.executemany(
-                """
-                    INSERT INTO edges (src, dst, delay_rise, delay_fall)
-                    VALUES (?, ?, ?, ?)
-                    """,
-                data,
-            )
+            connection.executemany(_SQL_INSERT_EDGE, data)
